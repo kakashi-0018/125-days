e3aa363c0af416417211ca3eaed7c4db76f0eed9
kakashi-0018 <naitiknehra11@gmail.com>
2026-09-18 23:17:11 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C="125dayshard-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","index.html","manifest.webmanifest","icon.svg","sw.js"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
